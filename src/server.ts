36e1398ed981ceff9a6c6faa58afe89490ecf1fd
devsujalpatel <sujal7455@gmail.com>
2025-06-04 23:11:44 +0530

    dot env configured

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 
+import config from '@/config';
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
 
-app.listen(3000, () => {
-  console.log(`Server is running on : http://localhost:3000`);
+app.get('/', (req, res) => {
+  res.json({
+    message: "Hello World!"
+  });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server is running on : http://localhost:${config.PORT}`);
 });
