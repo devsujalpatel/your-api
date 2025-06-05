12377b48e46a3bd54715e0df0b0f06bd1def0018
devsujalpatel <sujal7455@gmail.com>
2025-06-05 22:50:28 +0530

    random username

--- src/controllers/v1/auth/register.ts
+++ src/controllers/v1/auth/register.ts
@@ -1,24 +1,41 @@
 import { logger } from '@/lib/winston';
 import config from '@/config';
+import { getUername } from '@/utils';
+
 // Models
+import User from '@/models/user';
 
 // Types
-
 import type { Request, Response } from 'express';
+import type { IUser } from '@/models/user';
+
+type UserData = Pick<IUser, 'email' | 'password' | 'role'>;
 
 const register = async (req: Request, res: Response): Promise<void> => {
+  const { email, password, role } = req.body as UserData;
+
   try {
+    const username = getUername();
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+      role,
+    });
+
+
+    
     res.status(201).json({
       message: 'New user Created',
     });
   } catch (err) {
     res.status(500).json({
-        code: 'ServerError',
-        message: 'Internal Server Error',
-        error: err
-    })
-    logger.error('Error during user registeration', err)
+      code: 'ServerError',
+      message: 'Internal Server Error',
+      error: err,
+    });
+    logger.error('Error during user registeration', err);
   }
 };
 
-export default register
+export default register;
---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export const getUername = (): string => {
+  const usernamePrefix = 'user-';
+  const randomChars = Math.random().toString(36).slice(2);
+  const username = usernamePrefix + randomChars;
+  return username;
+};
