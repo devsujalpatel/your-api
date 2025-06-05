48eeb7f438e5f7ec0ace164103507d883a5a9082
devsujalpatel <sujal7455@gmail.com>
2025-06-05 22:41:15 +0530

    user model created

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,40 +11,90 @@ export interface IUser {
     website?: string;
     facebook?: string;
     instagram?: string;
+    linkedin?: string;
     x?: string;
     youtube?: string;
   };
 }
 
-const userSchema = new Schema<IUser>({
-  username: {
-    type: String,
-    required: [true, 'User is required'],
-    masLength: [20, 'Username must be less than 20 characters'],
-    unique: [true, 'Username must be unique'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    masLength: [20, 'Email must be less than 50 characters'],
-    unique: [true, 'Email must be unique'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    select: false,
-  },
-  role: {
-    type: String,
-    required: [true, 'Role is required'],
-    emum: {
-      values: ['admin', 'user'],
-      message: '{VALUE} is not supported',
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: [true, 'User is required'],
+      masLength: [20, 'Username must be less than 20 characters'],
+      unique: [true, 'Username must be unique'],
     },
-    default: 'user',
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      masLength: [20, 'Email must be less than 50 characters'],
+      unique: [true, 'Email must be unique'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      select: false,
+    },
+    role: {
+      type: String,
+      required: [true, 'Role is required'],
+      emum: {
+        values: ['admin', 'user'],
+        message: '{VALUE} is not supported',
+      },
+      default: 'user',
+    },
+    firstName: {
+      type: String,
+      maxlength: [20, 'First name must be less than 20 characters'],
+    },
+    lastName: {
+      type: String,
+      maxlength: [20, 'Last name must be less than 20 characters'],
+    },
+    socialLinks: {
+      website: {
+        type: String,
+        maxlength: [100, 'Website address must be less than 100 characters'],
+      },
+      facebook: {
+        type: String,
+        maxlength: [
+          100,
+          'Facebook profile url must be less than 100 characters',
+        ],
+      },
+      instagram: {
+        type: String,
+        maxlength: [
+          100,
+          'Instagram profile url must be less than 100 characters',
+        ],
+      },
+      linkedin: {
+        type: String,
+        maxlength: [
+          100,
+          'Linkedin profile url must be less than 100 characters',
+        ],
+      },
+      x: {
+        type: String,
+        maxlength: [100, 'X profile url must be less than 100 characters'],
+      },
+      youtube: {
+        type: String,
+        maxlength: [
+          100,
+          'Youtube channel url must be less than 100 characters',
+        ],
+      },
+    },
   },
-  firstName: {
-    type: String,
-    maxlength: [20, 'First name must be less than 20 characters'],
+  {
+    timestamps: true,
   },
-});
+);
+
+export default model<IUser>('User', userSchema);
