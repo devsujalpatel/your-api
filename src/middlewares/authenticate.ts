8f393f11b8a60ae002056678a091725fd3641422
Sujal Patel <sujal7455@gmail.com>
2025-06-12 03:33:35 +0530

    created types aunthenticate middleware error handling

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,18 +1,52 @@
-import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 
-import { verifyAccessToken } from "@/lib/jwt";
-import {logger} from "@/lib/winston";
+import { verifyAccessToken } from '@/lib/jwt';
+import { logger } from '@/lib/winston';
 
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from 'express';
 
-import type { Types } from "mongoose";
-
+import type { Types } from 'mongoose';
 
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
-const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-console.log(authHeader)
+  if (authHeader?.startsWith('Bearer ')) {
+    res.status(401).json({
+      code: 'AuthenticationError',
+      message: 'Access denied, no token provided',
+    });
+    return;
+  }
+  const [_, token]= authHeader?.split(' ') ?? [];
+  try {
+    const jwtPayload = verifyAccessToken(token) as {
+      userId: Types.ObjectId;
+    }
+    req.userId = jwtPayload.userId;
 
-}
+    return next();
+  } catch (err) {
+    if(err instanceof TokenExpiredError) {
+        res.status(401).json({
+          code: 'AuthenticationError',
+          message: 'Access token expired, please login again',
+        });
+        return;
+    }
+    if(err instanceof JsonWebTokenError) {
+        res.status(401).json({
+            code: 'AuthenticationError',
+            message: 'Invalid access token',
+        });
+        return;
+    }
+    res.status(500).json({
+        code: 'ServerError',
+        message: 'Internal Server Error',
+        error: err
+    });
+    logger.error('Error during authentication', err);
+  }
+};
 
-export default authenticate
+export default authenticate;
