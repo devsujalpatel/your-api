5af86b6177d8566fdec36cd8200df9d43e12cf11
Sujal Patel <sujal7455@gmail.com>
2025-06-12 03:40:29 +0530

    logout created with a bug

--- src/controllers/v1/auth/logout.ts
+++ src/controllers/v1/auth/logout.ts
@@ -1,15 +1,33 @@
-import { logger } from "@/lib/winston";
-import config from "@/config";
+import { logger } from '@/lib/winston';
+import config from '@/config';
 
-import Token from "@/models/token";
+import Token from '@/models/token';
 
 // Types
 
-import type { Request, Response } from "express";
+import type { Request, Response } from 'express';
 
 const logout = async (req: Request, res: Response): Promise<void> => {
   try {
+    const refreshToken = req.cookies.refreshToken as string;
+
+    if (!refreshToken) {
+      res.sendStatus(204);
+      return;
+    }
+    // Delete refresh token from db
+    await Token.deleteOne({ token: refreshToken });
+    logger.info('Refresh token deleted successfully', {
+      userId: req.userId,
+      token: refreshToken,
+    });
+    res.clearCookie('refreshToken', {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: 'strict',
+    });
     res.sendStatus(204);
+    logger.info('Logout successful', { userId: req.userId });
   } catch (err) {
     res.status(500).json({
       code: 'ServerError',
