1e252b7b3b7eb47fb79519fd19261fc7fa4deac2
Sujal Patel <sujal7455@gmail.com>
2025-06-13 00:37:19 +0530

    authorize middleware created

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -5,16 +5,18 @@ const router = Router();
 // Routes
 
 import authRoutes from '@/routes/v1/auth';
+import userRoutes from '@/routes/v1/auth'
 
 router.get('/', (req, res) => {
   res.status(200).json({
     message: 'API is Live',
     status: 'ok',
     version: '1.0.0',
-    timesamp: new Date().toISOString(),
+    timestamp: new Date().toISOString(),
   });
 });
 
 router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
 
 export default router;
--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,11 +1,13 @@
-import { Router } from "express";
-import {param, query, body} from "express-validator";
+import { Router } from 'express';
+import { param, query, body } from 'express-validator';
 
-import authenticate from "@/middlewares/authenticate";
-import validationError from "@/middlewares/validationError";
+import authenticate from '@/middlewares/authenticate';
+import validationError from '@/middlewares/validationError';
+import authorize from '@/middlewares/authorize';
 
-import User from '@/models/user'
+import User from '@/models/user';
 
 const router = Router();
+router.get('/current', authenticate, authorize(['admin', 'user']));
 
 export default router;
