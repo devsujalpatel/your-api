97107c8969df30f0e94e6f8697a10ee013c6745d
Sujal Patel <sujal7455@gmail.com>
2025-06-12 23:52:13 +0530

    user routes created

---
+++ src/routes/v1/user.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import {param, query, body} from "express-validator";
+
+import authenticate from "@/middlewares/authenticate";
+import validationError from "@/middlewares/validationError";
+
+import User from '@/models/user'
+
+const router = Router();
+
+export default router;
