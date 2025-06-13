d599e6da83cdb3cff7807f88a3e6eaa6451b891b
Sujal Patel <sujal7455@gmail.com>
2025-06-13 23:26:34 +0530

    username validation added for uniqueness

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -10,15 +10,31 @@ import User from '@/models/user';
 import getCurrentUser from '@/controllers/v1/user/get_current_user';
 import updateCurrentUser from '@/controllers/v1/user/update_current_user';
 
-
 const router = Router();
 router.get(
   '/current',
   authenticate,
   authorize(['admin', 'user']),
+  body('username')
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage('Username must be less than 20 characters')
+    .custom(async (value) => {
+      const userExists = await User.exists({ username: value });
+      if (userExists) {
+        throw new Error('Username is already taken');
+      }
+    }),
+  validationError,
   getCurrentUser,
 );
 
-router.put('/current', authenticate, authorize(['admin', 'user']), updateCurrentUser);
+router.put(
+  '/current',
+  authenticate,
+  authorize(['admin', 'user']),
+  updateCurrentUser,
+);
 
 export default router;
